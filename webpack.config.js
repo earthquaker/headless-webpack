9b6327ebfafc62ce52adb426729620ec941883a7
earthquaker <pe_lias@msn.com>
2016-11-15 11:25:20 +0100

    upd webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 var path = require('path');
+var fs = require('fs');
 
 var nodeModules = {};
+
+fs.readdirSync('node_modules')
+    .filter(function(x) {
+        return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function(mod) {
+        nodeModules[mod] = 'commonjs ' + mod;
+    });
 
 nodeModules['casper'] = 'commonjs casper';
 
